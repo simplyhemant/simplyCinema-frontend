a987754fa27c2c06e6af55d841ebde41e3292895
Hemant singh <171360192+simplyhemant@users.noreply.github.com>
2026-04-15 19:28:18 +0530

    fixed url

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,8 @@
 // CorsConfig.java allowedOrigins list.
 
 // const BASE_URL = "http://localhost:8080"; 
-const BASE_URL = "https://autos-antibody-usgs-tribune.trycloudflare.com";  // Deployed AWS Backend URL , cloudflare
+// const BASE_URL = "https://autos-antibody-usgs-tribune.trycloudflare.com";  // Deployed AWS Backend URL , cloudflare
+const BASE_URL = "https://simplycinema-backend.onrender.com";
 
 function getHeaders(isPublic = false) {
   const headers = {
